33b8f4b23b4a99b58b95451fb445aafdf6fce84c
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2022-09-07 19:03:33 +0100

    added position and artist

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,7 +1,11 @@
-const SongItem = function({song}){
-    console.log(song)
+const SongItem = function({song, listPosition}){
     return (
+        <>
+        <h3>{listPosition}</h3>
+        <h3>{song["im:artist"].label}</h3>
         <h3>{song.title.label}</h3>
+        <hr />
+        </>
     )
 };
 
--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -3,17 +3,14 @@ import SongItem from "./SongItem";
 const SongList = function({songs}){
     const songList = songs.map((song) => {
         return (
-            <SongItem song={song} />
+            <SongItem song={song} listPosition={songs.indexOf(song)+1} />
         )
     })
-
-    // .map((song) => {
-    //     console.log(song)
-    // })
 
     return (
         <>
         <h2>This is displayed on songList.jsx</h2>
+        <hr />
         {songList}
         </>
     )
